c2968fa441a6aff0b84fe7e39ed46aea10459f31
Ribrumfield <ribrumfield@NN123-HA-08.HS.sd203.org>
2016-01-11 10:28:20 -0600

    worked on the picture lab

--- PictureLab/classes/PictureTester.java
+++ PictureLab/classes/PictureTester.java
@@ -16,23 +16,6 @@ public class PictureTester
     beach.zeroBlue();
     beach.explore();
   }
-  
-  public void testKeepOnlyBlue()
-
-   {
-    Pixel[][] pixels = this.getPixels2D();
-
-     for (Pixel[] rowArray : pixels)
-    
-    {    
-        for (Pixel pixelObj : rowArray)
-    
-        {
-            pixelObj.setRed(0);
-            pixelObj.setGreen(0);
-        }
-    }
-    }
   /** Method to test mirrorVertical */
   public static void testMirrorVertical()
   {
@@ -40,6 +23,29 @@ public class PictureTester
     caterpillar.explore();
     caterpillar.mirrorVertical();
     caterpillar.explore();
+    
+    Pixel[][] pixels = this.getPixels2D();
+        
+    Pixel leftPixel = null;
+    
+    Pixel rightPixel = null;
+    
+    int width = pixels[0].length;
+    
+    for (int row = 0; row < pixels.length; row++)
+    
+    {
+       for (int col = 0; col < width / 2; col++)
+       {
+        
+           leftPixel = pixels[row][col];
+           
+           rightPixel = pixels[row][width ? 1 - col]:
+            
+           rightPixel.setColor(leftPixel.getColor());
+    
+       }
+    } 
   }
   
   /** Method to test mirrorTemple */
